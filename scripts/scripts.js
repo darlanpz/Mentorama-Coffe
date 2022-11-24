66149bcedf7e02bb831c8f4c3af0e7f404608c7b
Darlan Paz <darlan.mpaz@outlook.com>
2022-11-24 18:22:53 -0300

    Reestruturação do CSS, hero-section-concluida

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,11 @@
-
 // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navmenu").style.background = "black";
+    document.getElementById("navmenu").style.background = "rgba(0,0,0,0.8)";
   } else {
     document.getElementById("navmenu").style.background = "none";
   }
